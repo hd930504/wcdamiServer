e9d5fc61234b5045a41858d1e8413d76f8be0934
hd930504 <254564964@qq.com>
2019-07-20 18:52:03 +0800

    1

--- routes/api.js
+++ routes/api.js
@@ -29,28 +29,36 @@ router.get('/banner',(req,res)=>{
             res.status(500).send('database error').end();
         }else{
             for(let i in data){
-                data[i].src = data[i].src;
+                data[i].src = 'http://'+req.headers.host+'/images/'+data[i].src;
             }
             res.json(data).end();
         }
     })
 })
-router.post('/uploadBanner',(req,res)=>{
-    var form = new formidable.IncomingForm();
-    form.parse(req,(err,fields,files)=>{
-        if(err){res.send(err)}
-        console.log(files.File.type)
-        // let imgPath = files.path
-        // let imgName = './test.' + files.type.split('/')[1];
-        // let data = fs.readFileSync(imgPath);
-
-        // fs.writeFile(imgName,data,function(err){
-        //     if(err){return console.log(err)}
-
-        //     res.json({code:1})
-        // })
+router.get('/goods',(req,res)=>{
+    db.query(`SELECT * FROM goodstable`,(err,data)=>{
+        if(err){
+            console.error(err);
+            res.status(500).send('database error').end();
+        }else{
+            for(let i in data){
+                data[i].goodsImg = 'http://'+req.headers.host+'/images/'+data[i].goodsImg;
+            }
+            res.json(data).end();
+        }
     })
 })
+router.get('/news',(req,res)=>{
+    db.query(`SELECT * FROM newstable`,(err,data)=>{
+        if(err){
+            console.error(err);
+            res.status(500).send('database error').end();
+        }else{
+            res.json(data).end();
+        }
+    })
+})
+
 router.get("/gt/register", function (req, res) {
     // 向极验申请每次验证所需的challenge
     captcha.register({
